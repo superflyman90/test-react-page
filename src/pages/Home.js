aafdab9bbb3b0c3886bc509d368cf0327ff2b6d1
superflyman90 <superflyman90@gmail.com>
2022-06-27 20:40:34 +0200

    css Home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,8 +3,14 @@ import React from 'react';
 const Home = () => {
     return (
         <section id="hero" className="hero">
-            <div className="container">
-                <h1>Home</h1>
+            <h2 className='hero__title'>Home</h2>
+            <div className="container hero__section">
+                <div className="hero__left">
+                    left
+                </div>
+                <div className="hero__right">
+                    right
+                </div>
             </div>
         </section>
     );
