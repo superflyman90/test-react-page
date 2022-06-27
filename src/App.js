ee6acc5660615c5e9e83eb50b9075dc9304b0db4
superflyman90 <superflyman90@gmail.com>
2022-06-27 19:19:06 +0200

    Ajout des pages et du css correspondant

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
+import Contact from './pages/Contact';
 import Home from './pages/Home';
+import Portfolio from './pages/Portfolio';
 
 const App = () => {
     return (
@@ -9,6 +11,8 @@ const App = () => {
             <Navbar />
             <Routes>
                 <Route path="/" element={<Home />} />
+                <Route path="/portfolio" element={<Portfolio />} />
+                <Route path="/contact" element={<Contact />} />
             </Routes>
         </div>
     );
---
+++ src/pages/Portfolio.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Portfolio = () => {
+    return (
+        <>
+            <section id="portfolio" className="portfolio">
+                <div className="container">
+                    <h2>Portfolio</h2>
+                </div>
+            </section>
+        </>
+    );
+};
+
+export default Portfolio;
